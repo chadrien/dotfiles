11a4ac2242083c4eb4063d1d33fe22f34a422236
Hadrien de Cuzey <hadrien@platinium-group.org>
2016-05-10 17:36:37 +0200

    No more ruby module

--- modules/chadrien/manifests/ruby.pp
+++ modules/chadrien/manifests/ruby.pp
@@ -1,10 +1,16 @@
 include homebrew
 
+define ruby_gem {
+  exec { "install gem ${name}":
+    command => "gem install ${name}",
+    unless => "gem install ${name}"
+  }
+}
+
 class chadrien::ruby {
   package { 'ruby':
     ensure => present
   }
-  class { 'ruby::global':
-    version => 'system'
-  }
+
+  ruby_gem { 'bundler':; }
 }
