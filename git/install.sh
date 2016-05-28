054ad5f68e24313334bb2c4181cd6995f788c507
chadrien <chadrien@chadrien.fr>
2016-05-28 21:09:04 +0200

    Add gitconfig generation

--- git/install.sh
+++ git/install.sh
@@ -6,4 +6,15 @@ if [ "`uname`" = 'Darwin' ]; then
   [ ! -d "`brew --prefix hub`" ] && brew install hub
 fi
 
-(cd $DOTFILES_ROOT/git && stow -t $CONFIG_DIR stow)
+[ ! -d "$DOTFILES_ROOT/git/stow/home" ] && mkdir $DOTFILES_ROOT/git/stow/home
+
+if [ ! -f "$DOTFILES_ROOT/git/stow/home/.gitconfig" ]; then
+  read -p 'Your git usename: ' username
+  read -p 'Your git email: ' email
+  sed \
+    -e "s/USERNAME/${username}/" \
+    -e "s/EMAIL/${email}/" \
+    $DOTFILES_ROOT/git/gitconfig.template > $DOTFILES_ROOT/git/stow/home/.gitconfig
+fi
+
+(cd $DOTFILES_ROOT/git/stow && stow -t $CONFIG_DIR conf.d && stow -t $HOME home)
