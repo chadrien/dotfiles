2924e5c8794cf1fc9e20406d86a8e55369ab27c8
chadrien <chadrien@chadrien.fr>
2016-06-01 11:37:34 +0200

    Move osx related stuff to its own file

---
+++ git/install.osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[ "`uname`" != 'Darwin' ] && return
+
+[ ! -d "`brew --prefix git`" ] && brew install git
+[ ! -d "`brew --prefix gnupg`" ] && brew install gnupg
+[ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
+[ ! -d "`brew --prefix hub`" ] && brew install hub
--- git/install.sh
+++ git/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ "`uname`" = 'Darwin' ]; then
-  [ ! -d "`brew --prefix git`" ] && brew install git
-  [ ! -d "`brew --prefix gnupg`" ] && brew install gnupg
-  [ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
-  [ ! -d "`brew --prefix hub`" ] && brew install hub
-fi
-
 [ ! -d "$DOTFILES_ROOT/git/stow/home" ] && mkdir $DOTFILES_ROOT/git/stow/home
 
 if [ ! -f "$DOTFILES_ROOT/git/stow/home/.gitconfig" ]; then
