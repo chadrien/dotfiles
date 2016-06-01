44b655b55324b94d1c94ca4c40cea9c2f084dabb
chadrien <chadrien@chadrien.fr>
2016-06-02 00:01:10 +0200

    Use pinentry-mac otherwise the gpg-agent won't start

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -4,5 +4,10 @@
 
 [ ! -d "`brew --prefix git`" ] && brew install git
 [ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
+[ ! -d "`brew --prefix pinentry-mac`" ] && brew install pinentry-mac
 [ ! -d "`brew --prefix hub`" ] && brew install hub
 ! sudo gem list -i git-up >/dev/null 2>&1 && sudo gem install git-up
+
+[ ! -d "$HOME/.gnupg" ] && mkdir $HOME/.gnupg
+
+(cd $DOTFILES_ROOT/git/stow && stow -t $HOME/.gnupg .gnupg)
