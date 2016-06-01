74cbbf81d2b2e19576994d11ef4240de8550b719
chadrien <chadrien@chadrien.fr>
2016-06-01 23:55:51 +0200

    Use gpg2, so remove gnupg

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -3,7 +3,6 @@
 [ "`uname`" != 'Darwin' ] && return
 
 [ ! -d "`brew --prefix git`" ] && brew install git
-[ ! -d "`brew --prefix gnupg`" ] && brew install gnupg
 [ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
 [ ! -d "`brew --prefix hub`" ] && brew install hub
 ! sudo gem list -i git-up >/dev/null 2>&1 && sudo gem install git-up
