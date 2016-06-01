bc6d5b0e89dfca9b95888c7209249dc941d6da05
chadrien <chadrien@chadrien.fr>
2016-06-01 12:02:38 +0200

    Install git-up

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -6,3 +6,4 @@
 [ ! -d "`brew --prefix gnupg`" ] && brew install gnupg
 [ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
 [ ! -d "`brew --prefix hub`" ] && brew install hub
+! sudo gem list -i git-up && sudo gem install git-up
