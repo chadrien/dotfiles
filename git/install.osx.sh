805cd869a4e0545c439f9e04ec98a3747ffcfe37
chadrien <chadrien@chadrien.fr>
2016-06-01 23:53:02 +0200

    Dont output the command to test if a gem is installed

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -6,4 +6,4 @@
 [ ! -d "`brew --prefix gnupg`" ] && brew install gnupg
 [ ! -d "`brew --prefix gpg2`" ] && brew install gpg2
 [ ! -d "`brew --prefix hub`" ] && brew install hub
-! sudo gem list -i git-up && sudo gem install git-up
+! sudo gem list -i git-up >/dev/null 2>&1 && sudo gem install git-up
