2b8933107207f37bca23c9732e56268c788745a8
chadrien <chadrien@chadrien.fr>
2016-06-03 17:12:12 +0200

    General improvement of git management
    * do not manage ~/.gitconfig but create a (almost) blank copy to allow user customisation and still have a shared, git tracked configuration
    * use gpg2 as gpg program
    * misc configuration

--- git/install.sh
+++ git/install.sh
@@ -2,13 +2,11 @@
 
 [ ! -d "$DOTFILES_ROOT/git/stow/home" ] && mkdir $DOTFILES_ROOT/git/stow/home
 
-if [ ! -f "$DOTFILES_ROOT/git/stow/home/.gitconfig" ]; then
-  read -p 'Your git usename: ' username
-  read -p 'Your git email: ' email
-  sed \
-    -e "s/USERNAME/${username}/" \
-    -e "s/EMAIL/${email}/" \
-    $DOTFILES_ROOT/git/gitconfig.template > $DOTFILES_ROOT/git/stow/home/.gitconfig
-fi
-
 (cd $DOTFILES_ROOT/git/stow && stow -t $CONFIG_DIR conf.d && stow -t $HOME home)
+
+PATH=$DOTFILES_ROOT/vendors/crudini:$PATH
+
+[ ! -f "$HOME/.gitconfig" ] && cat <<EOF > "$HOME/.gitconfig"
+[include]
+	path = ~/.gitconfig.shared
+EOF
