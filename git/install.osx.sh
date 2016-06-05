192340e015efba790f2c8afc9b226d5c6e705f11
chadrien <chadrien@chadrien.fr>
2016-06-05 19:59:43 +0200

    Install git-up on all platforms

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -2,8 +2,6 @@
 
 [ "`uname`" != 'Darwin' ] && return
 
-! sudo gem list -i git-up >/dev/null 2>&1 && sudo gem install git-up
-
 [ ! -d "$HOME/.gnupg" ] && mkdir $HOME/.gnupg
 
 (cd $DOTFILES_ROOT/git/stow && stow -t $HOME/.gnupg .gnupg)
--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+! sudo gem list -i git-up >/dev/null 2>&1 && sudo gem install git-up
+
 [ ! -d "$DOTFILES_ROOT/git/stow/home" ] && mkdir $DOTFILES_ROOT/git/stow/home
 
 (cd $DOTFILES_ROOT/git/stow && stow -t $CONFIG_DIR conf.d && stow -t $HOME home)
