bacf9d5479fdd0cb8e9807ff1013e709bfea3e83
chadrien <chadrien@chadrien.fr>
2016-06-06 17:54:50 +0200

    Revert "Add vim suggar to atom"
    This reverts commit 45e75b1c5946ecfe8fb02e56f38d772989baf25e.

--- apps/install.sh
+++ apps/install.sh
@@ -6,9 +6,6 @@
 ! apm list | grep project-plus >/dev/null && apm install project-plus
 ! apm list | grep data-atom >/dev/null && apm install data-atom
 ! apm list | grep seti-ui >/dev/null && apm install seti-ui
-! apm list | grep vim-mode >/dev/null && apm install vim-mode
-! apm list | grep ex-mode >/dev/null && apm install ex-mode
-! apm list | grep relative-numbers >/dev/null && apm install relative-numbers
 ! apm list | grep expand-region >/dev/null && apm install expand-region
 ! apm list | grep next-line-comment >/dev/null && apm install next-line-comment
 
