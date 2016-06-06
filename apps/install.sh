45e75b1c5946ecfe8fb02e56f38d772989baf25e
chadrien <chadrien@chadrien.fr>
2016-06-06 14:21:09 +0200

    Add vim suggar to atom

--- apps/install.sh
+++ apps/install.sh
@@ -6,5 +6,8 @@
 ! apm list | grep project-plus >/dev/null && apm install project-plus
 ! apm list | grep data-atom >/dev/null && apm install data-atom
 ! apm list | grep seti-ui >/dev/null && apm install seti-ui
+! apm list | grep vim-mode >/dev/null && apm install vim-mode
+! apm list | grep ex-mode >/dev/null && apm install ex-mode
+! apm list | grep relative-numbers >/dev/null && apm install relative-numbers
 
 (cd $DOTFILES_ROOT/apps/stow && stow -t $HOME home)
