a9055ed86b75d60e8fb8cb18af40800490fec6ed
chadrien <chadrien@chadrien.fr>
2016-06-09 11:09:59 +0200

    Replace module with more active fork

--- apps/install.sh
+++ apps/install.sh
@@ -2,7 +2,7 @@
 
 # atom
 ! apm list | grep editorconfig >/dev/null && apm install editorconfig
-! apm list | grep terminal-plus >/dev/null && apm install terminal-plus
+! apm list | grep platformio-ide-terminal >/dev/null && apm install platformio-ide-terminal
 ! apm list | grep project-plus >/dev/null && apm install project-plus
 ! apm list | grep data-atom >/dev/null && apm install data-atom
 ! apm list | grep seti-ui >/dev/null && apm install seti-ui
