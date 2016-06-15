eaf704def239b1447d877c9262a42e3b57a70064
chadrien <chadrien@chadrien.fr>
2016-06-15 10:32:54 +0200

    Use better diff highlight for git

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -5,3 +5,5 @@
 [ ! -d "$HOME/.gnupg" ] && mkdir $HOME/.gnupg
 
 (cd $DOTFILES_ROOT/git/stow && stow -t $HOME/.gnupg .gnupg)
+
+ln -s $HOMEBREW_ROOT/opt/git/share/git-core/contrib/diff-highlight/diff-highlight $HOMEBREW_ROOT/bin/
