13ee975dd2f3d43be9f4d3194e76c937b9a573c5
chadrien <chadrien@chadrien.fr>
2016-06-16 14:23:50 +0200

    git: only link diff-highlight if not already linked

--- git/install.osx.sh
+++ git/install.osx.sh
@@ -6,4 +6,4 @@
 
 (cd $DOTFILES_ROOT/git/stow && stow -t $HOME/.gnupg .gnupg)
 
-ln -s $HOMEBREW_ROOT/opt/git/share/git-core/contrib/diff-highlight/diff-highlight $HOMEBREW_ROOT/bin/
+[ ! -e $HOMEBREW_ROOT/bin/diff-highlight ] && ln -s $HOMEBREW_ROOT/opt/git/share/git-core/contrib/diff-highlight/diff-highlight $HOMEBREW_ROOT/bin/
