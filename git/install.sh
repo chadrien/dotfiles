deef227ceb2ba82afe7c322964f16107558ca356
chadrien <chadrien@chadrien.fr>
2016-06-22 12:35:46 +0200

    Remove dead code

--- git/install.sh
+++ git/install.sh
@@ -6,8 +6,6 @@
 
 (cd $DOTFILES_ROOT/git/stow && stow -t $CONFIG_DIR conf.d && stow -t $HOME home)
 
-PATH=$DOTFILES_ROOT/vendors/crudini:$PATH
-
 [ ! -f "$HOME/.gitconfig" ] && cat <<EOF > "$HOME/.gitconfig"
 [include]
 	path = ~/.gitconfig.shared
