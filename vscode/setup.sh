291b2424aadb86472c6fb0fc8bf1df0e5d18826b
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2016-12-28 14:45:01 +0200

    Add new vscode extension

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 extenstions=(EditorConfig.EditorConfig akamud.vscode-theme-onedark alefragnani.project-manager dbaeumer.vscode-eslint maptz.camelcasenavigation)
-extenstions+=(deerawan.vscode-dash)
+extenstions+=(deerawan.vscode-dash christian-kohler.path-intellisense)
 
 for extenstion in "${extenstions[@]}"; do
   code --install-extension $extenstion
