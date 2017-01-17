f6a1f0cee92ce01e05ee960deae5c3698542c7d6
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-01-17 09:18:01 +0200

    Remove ubersicht

--- kwm/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-[ ! -f "$HOME/Library/Application Support/Übersicht/widgets/focused-window.coffee" ] && rm -rf "$HOME/Library/Application Support/Übersicht/widgets" && git clone https://github.com/chadrien/nerdbar.widget.git "$HOME/Library/Application Support/Übersicht/widgets"
