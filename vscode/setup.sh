6f6c54a5c4653c46849d1f6fd9e969de769e7352
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-01-31 17:42:12 +0200

    More arch stuff

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 extenstions=(EditorConfig.EditorConfig akamud.vscode-theme-onedark alefragnani.project-manager dbaeumer.vscode-eslint maptz.camelcasenavigation)
-extenstions+=(deerawan.vscode-dash christian-kohler.path-intellisense patbenatar.advanced-new-file TeddyDD.fish sandy081.todotasks vscodevim.vim)
+extenstions+=(silverlakesoftware.searchdocsets-vscode christian-kohler.path-intellisense patbenatar.advanced-new-file TeddyDD.fish)
+extenstions+=(sandy081.todotasks vscodevim.vim)
 
 for extenstion in "${extenstions[@]}"; do
   code --install-extension $extenstion
