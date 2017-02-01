c4bf11db2fc67752c753055aa9112d5c1c55a94c
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-02-01 15:30:45 +0200

    Even better arch support

--- install.arch.sh
+++ install.arch.sh
@@ -6,14 +6,21 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 export DOTFILES_ROOT=`dirname $0`
 
-# install stow
-yaourt -S --needed stow
+# install packages
+packages=()
+for package in $(cat $DOTFILES_ROOT/Archfile | grep -v '^#'); do
+  packages+=($package)
+done
+yaourt -S --needed ${packages[@]}
 
 # run setups
 for dir in $(find $DOTFILES_ROOT -maxdepth 1 -type d -not -name 'stow*' -not -name 'homebrew'); do
   [ -f "${dir}/setup.sh" ] && source "${dir}/setup.sh"
   [ -f "${dir}/setup.arch.sh" ] && source "${dir}/setup.arch.sh"
 done
+
+# cleanup
+yaourt -Qdt
 
 (cd $DOTFILES_ROOT; stow -t $HOME stow)
 (cd $DOTFILES_ROOT; stow -t $HOME stow-arch)
--- tmuxinator/setup.arch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-yaourt -S --needed tmuxinator
