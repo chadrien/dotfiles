bd4a8db056547a1f373411dc978f0d910565a1b3
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-07-04 14:05:12 +0300

    Commit stuff

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 extenstions=(EditorConfig.EditorConfig alefragnani.project-manager dbaeumer.vscode-eslint maptz.camelcasenavigation)
-extenstions+=(patbenatar.advanced-new-file steoates.autoimport eg2.tslint eamodio.gitlens)
+extenstions+=(patbenatar.advanced-new-file eg2.tslint eamodio.gitlens)
 
 for extenstion in "${extenstions[@]}"; do
   code --install-extension $extenstion
